c3e2535e880a413832621f0e93cf07a0f6994d32
QuantumParadigm2024 <quantumshare12@gmail.com>
2025-06-23 15:38:18 +0530

    Receipt Extractor Backend

---
+++ receipt-extractor-backend/src/main/java/com/sleek/receipt_backend/ReceiptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sleek.receipt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptBackendApplication.class, args);
+	}
+
+}
---
+++ receipt-extractor-backend/src/test/java/com/sleek/receipt_backend/ReceiptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleek.receipt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
